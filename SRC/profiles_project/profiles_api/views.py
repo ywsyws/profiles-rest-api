e683282e74fc74d0a5f8bc711c6c92524cae8977
channing <channing.platevoet@gmail.com>
2019-03-05 23:04:32 +0100

    Added POST, PUT, PATCH and DELETE functions to our APIView

--- SRC/profiles_project/profiles_api/views.py
+++ SRC/profiles_project/profiles_api/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from . import serializers
+from rest_framework import status
+
 # Create your views here.
 
 class HelloApiView(APIView):
     """Test API View."""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features."""
@@ -19,3 +24,39 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Creat a hello message with our name."""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}!'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handles updating an object."""
+
+        return Response({'method':'put'})
+
+    #def patch(self, request, pk=None):
+        """Patch request, only updates fields provided in the request."""
+        """this example missing the method to actually update the differences \
+        between what's been provided in the request"""
+
+        #return Response({'method':'patch'})
+
+    def patch(self, request, pk=None):
+        """Patch request, only updates fields provided in the request."""
+
+        return Response({'method': 'patch'})
+
+
+    def delete(self, request, pk=None):
+        """Delete an object."""
+
+        return Response({'method':'delete'})
