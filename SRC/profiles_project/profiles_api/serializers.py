ff08d5e8bcaef815183fbffe04284a2067019c61
channing <channing.platevoet@gmail.com>
2019-03-12 16:07:49 +0100

    Added improved permissions to use profile feed items API.

--- SRC/profiles_project/profiles_api/serializers.py
+++ SRC/profiles_project/profiles_api/serializers.py
@@ -28,3 +28,11 @@ class UserProfileSerializer(serializers.ModelSerializer):
         user.save()
 
         return user
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """A serializer for prifle feed items."""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
